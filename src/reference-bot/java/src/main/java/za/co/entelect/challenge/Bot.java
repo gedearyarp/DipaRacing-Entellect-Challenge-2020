dd89707eead2b8806b0f482233da43c363a6a2fc
arikrayi <83354451+arikrayi@users.noreply.github.com>
2022-02-14 02:26:21 +0700

    mantap bos

--- src/reference-bot/java/src/main/java/za/co/entelect/challenge/Bot.java
+++ src/reference-bot/java/src/main/java/za/co/entelect/challenge/Bot.java
@@ -7,6 +7,8 @@ import za.co.entelect.challenge.enums.Terrain;
 
 import java.util.*;
 
+import javax.management.Descriptor;
+
 import static java.lang.Math.max;
 
 import java.security.SecureRandom;
@@ -19,14 +21,25 @@ public class Bot {
     private final Random random;
 
     private final static Command ACCELERATE = new AccelerateCommand();
+    private final static Command DECELERATE = new DecelerateCommand();
     private final static Command LIZARD = new LizardCommand();
     private final static Command OIL = new OilCommand();
     private final static Command BOOST = new BoostCommand();
     private final static Command EMP = new EmpCommand();
     private final static Command FIX = new FixCommand();
-
+    private final static Command DO_NOTHING = new DoNothingCommand();
     private final static Command TURN_RIGHT = new ChangeLaneCommand(1);
     private final static Command TURN_LEFT = new ChangeLaneCommand(-1);
+    
+    private float weightTruck = 999;
+    private float weightWall = 50;
+    private float weightOil = 20;
+    private float weightMud = 20;
+    private float weightFinish = -333;
+    
+    private int[] nextSpeed = {3, 0, 0, 5, 0, 6, 8, 0, 9, 9, 0, 0, 0, 0, 0, 15};
+    private int[] prevSpeed = {0, 0, 0, 0, 0, 3, 5, 0, 6, 8, 0, 0, 0, 0, 0, 9};
+    
 
     public Bot() {
         this.random = new SecureRandom();
@@ -39,61 +52,92 @@ public class Bot {
         Car opponent = gameState.opponent;
 
         //Basic fix logic
-        List<Object> blocks = getBlocksInFront(myCar.position.lane, myCar.position.block, gameState);
-        List<Object> nextBlocks = blocks.subList(0,1);
-
-        //Fix first if too damaged to move
-        if(myCar.damage == 5) {
+        // List<Object> blocks = getBlocksInFront(myCar.position.lane, myCar.position.block, gameState);
+        // List<Object> nextBlocks = blocks.subList(0,1);
+        
+        //Fix
+        if(myCar.damage >= 5) {
             return FIX;
         }
 
-        //Basic avoidance logic
-        if (blocks.contains(Terrain.MUD) || nextBlocks.contains(Terrain.WALL)) {
-            if (hasPowerUp(PowerUps.LIZARD, myCar.powerups)) {
-                return LIZARD;
+        if (myCar.position.block < opponent.position.block){
+            // emp jedar jedar atau accelerate
+            int acc = nextSpeed[myCar.speed] - opponent.speed;
+            if (hasPowerUp(PowerUps.BOOST, myCar.powerups)){
+                acc = 15 - opponent.speed;
             }
-            if (nextBlocks.contains(Terrain.MUD) || nextBlocks.contains(Terrain.WALL)) {
-                int i = random.nextInt(directionList.size());
-                return directionList.get(i);
+            //emp perlu merhatiin speed kita setelah lurus kalo ada nabrak
+            int emp = myCar.speed - 3;
+            if (acc > emp){
+                return move(gameState);
+            }
+            else{
+                if (hasPowerUp(PowerUps.EMP, myCar.powerups)){
+                    //speed minimum supaya worth it nembak emp
+                    if (Math.abs(myCar.position.lane - opponent.position.lane) <= 1 
+                        && countWeight(myCar.position.lane, myCar.position.block, myCar.speed, gameState) < 50){
+                        return EMP;
+                    }
+                }
+                return move(gameState);
             }
         }
+        else if (myCar.position.block == opponent.position.block) {
+            move(gameState);
+        }
+        else if (myCar.position.block <= opponent.position.block + 20){
+            //pake oil
+            move(gameState);
+        }
+        else {
+            //pake tweet
+            move(gameState);
+        }
+
+        
+        //Basic avoidance logic
+        // if (blocks.contains(Terrain.MUD) || nextBlocks.contains(Terrain.WALL)) {
+        //     if (hasPowerUp(PowerUps.LIZARD, myCar.powerups)) {
+        //         return LIZARD;
+        //     }
+        //     if (nextBlocks.contains(Terrain.MUD) || nextBlocks.contains(Terrain.WALL)) {
+        //         int i = random.nextInt(directionList.size());
+        //         return directionList.get(i);
+        //     }
 
 
         //Basic improvement logic
-        if (hasPowerUp(PowerUps.BOOST, myCar.powerups)) {
-            return BOOST;
-        }
+        // if (hasPowerUp(PowerUps.BOOST, myCar.powerups)) {
+        //     return BOOST;
+        // }
 
         //Accelerate first if going to slow
-        if(myCar.speed <= 3) {
-            return ACCELERATE;
-        }
+        // if(myCar.speed <= 3) {
+        //     return ACCELERATE;
+        // }
 
         //Basic fix logic
-        if(myCar.damage >= 2 && myCar.speed <= 3) {
-            return FIX;
-        }
 
-        if(hasPowerUp(PowerUps.EMP, myCar.powerups) && myCar.position.block < opponent.position.block){
-            if(Math.abs(myCar.position.lane - opponent.position.lane) <= 1){
-                //jangan sampe collision, harus diimplemen
-                return EMP;
-            }
-        }
+        // if(hasPowerUp(PowerUps.EMP, myCar.powerups) && myCar.position.block < opponent.position.block){
+        //     if(Math.abs(myCar.position.lane - opponent.position.lane) <= 1){
+        //         //jangan sampe collision, harus diimplemen
+        //         return EMP;
+        //     }
+        // }
 
-        if(hasPowerUp(PowerUps.OIL, myCar.powerups)){
-            if(myCar.position.lane == opponent.position.lane){
-                if(myCar.position.block > opponent.position.block && myCar.position.block <= opponent.position.block + 15){
-                    return OIL;
-                }
-            }
-        }
+        // if(hasPowerUp(PowerUps.OIL, myCar.powerups)){
+        //     if(myCar.position.lane == opponent.position.lane){
+        //         if(myCar.position.block > opponent.position.block && myCar.position.block <= opponent.position.block + 15){
+        //             return OIL;
+        //         }
+        //     }
+        // }
 
         //Pake TWEET
 
-        if(countPowerUp(PowerUps.OIL, myCar.powerups) > 3){
-            return OIL;
-        }
+        // if(countPowerUp(PowerUps.OIL, myCar.powerups) > 3){
+        //     return OIL;
+        // }
 
         // //Basic avoidance logic
         // if (blocks.contains(Terrain.MUD) || nextBlocks.contains(Terrain.WALL)) {
@@ -116,7 +160,6 @@ public class Bot {
         //         return EMP;
         //     }
         // }
-
         return ACCELERATE;
     }
 
@@ -137,6 +180,110 @@ public class Bot {
             }
         }
         return count;
+    }
+
+    private Command accelerate(GameState gameState){
+        Car myCar = gameState.player;
+        int[] maxSpeed = {15, 9, 8, 6, 3, 0};
+        if(hasPowerUp(PowerUps.BOOST, myCar.powerups)){
+            if (myCar.damage == 0){
+                return BOOST;
+            }
+            else {
+                return FIX;
+            }
+        }
+        if(myCar.speed == maxSpeed[myCar.damage] && myCar.damage >= 2){
+            if(myCar.damage >= 2){
+                return FIX;
+            }
+            else{
+                return offensive(gameState);
+            }
+        }
+        return ACCELERATE;
+    }
+
+    private Command move(GameState gameState){
+        Car myCar = gameState.player;
+        float weightKiri = countWeight(myCar.position.lane-1, myCar.position.block, myCar.speed - 1, gameState),
+            weightLurus = countWeight(myCar.position.lane, myCar.position.block, myCar.speed, gameState),
+            weightKanan = countWeight(myCar.position.lane+1, myCar.position.block, myCar.speed-1, gameState),
+            weightUjungTengah = countWeight(myCar.position.lane, myCar.position.block+myCar.speed-1, 1, gameState),
+            weightAccelerate = countWeight(myCar.position.lane, myCar.position.block, nextSpeed[myCar.speed], gameState),
+            weightDecelerate = countWeight(myCar.position.lane, myCar.position.block, prevSpeed[myCar.speed], gameState);
+            
+        if (weightAccelerate <= 0){
+            return accelerate(gameState);
+        }
+        else if (weightLurus <= 0 && weightAccelerate > 0){
+            return offensive(gameState);
+        }
+        else if (weightKanan <= 0 && weightKiri <= 0) {
+            if (myCar.position.lane <= 2) return TURN_RIGHT;
+            else return TURN_LEFT;
+        }
+        else if (weightKanan <= 0){
+            return TURN_RIGHT;
+        }
+        else if (weightKiri <= 0){
+            return TURN_LEFT;
+        }
+        else {
+            if (weightUjungTengah == 0 && hasPowerUp(PowerUps.LIZARD, myCar.powerups)){
+                    return LIZARD;
+            }
+            else{
+                if (weightUjungTengah <= weightKanan && weightUjungTengah <= weightKiri 
+                && weightUjungTengah < weightDecelerate && hasPowerUp(PowerUps.LIZARD, myCar.powerups)){
+                    return LIZARD;
+                }
+                else if (weightLurus <= weightKanan && weightLurus <= weightKiri && weightLurus == weightDecelerate){
+                    return offensive(gameState);
+                }
+                else if (weightKanan == weightKiri && weightKiri < weightLurus && weightKiri <= weightDecelerate){
+                    if (myCar.position.lane <= 2) return TURN_RIGHT;
+                    else return TURN_LEFT;
+                }
+                else if (weightKanan <= weightUjungTengah && weightKanan < weightKiri && weightKanan <= weightDecelerate){
+                    return TURN_RIGHT;
+                }
+                else if (weightKiri < weightKanan && weightKiri <= weightDecelerate && weightKiri <= weightUjungTengah){
+                    return TURN_LEFT;
+                }
+                else return DECELERATE;
+            }
+        }
+    }
+
+
+    private Command offensive(GameState gameState){
+        Car myCar = gameState.player;
+        Car opponent = gameState.opponent;
+        
+        if (myCar.position.block < opponent.position.block){
+            if (hasPowerUp(PowerUps.EMP, myCar.powerups)){
+                //speed minimum supaya worth it nembak emp
+                if(Math.abs(myCar.position.lane - opponent.position.lane) <= 1){
+                    return EMP;
+                }
+            }
+        }
+        else if (myCar.position.block == opponent.position.block) {
+            return DO_NOTHING;
+        }
+        else if (myCar.position.block <= opponent.position.block + 20){
+            if(hasPowerUp(PowerUps.OIL, myCar.powerups)){
+                return OIL;
+            }
+        }
+        else {
+            return new TweetCommand(opponent.position.lane, opponent.position.block + opponent.speed + 5);
+            // if(countPowerUp(PowerUps.OIL, myCar.powerups) > 3){
+            //     return OIL;
+            // }
+        }
+        return DO_NOTHING;
     }
 
     /**
@@ -160,4 +307,40 @@ public class Bot {
         return blocks;
     }
 
+    private float countWeight(int lane, int block, int speed, GameState gameState) {
+        if(lane <= 0 || lane >= 5) return 99999;
+
+        List<Lane[]> map = gameState.lanes;
+        int startBlock = map.get(0)[0].position.block;
+        int finishBlock = 0, oilBlock = 0, mudBlock = 0, wallBlock = 0, truckBlock = 0;
+
+        Lane[] laneList = map.get(lane - 1);
+        for (int i = block - startBlock + 1; i <= block - startBlock + speed; i++) {
+            if (laneList[i].terrain == Terrain.FINISH) {
+                finishBlock++;
+                break;
+            }
+            if (laneList[i].isTruck) {
+                truckBlock++;
+                continue;
+            }
+            if (laneList[i].terrain == Terrain.OIL_SPILL) {
+                oilBlock++;
+            }
+            if (laneList[i].terrain == Terrain.WALL) {
+                wallBlock++;
+            }
+            if (laneList[i].terrain == Terrain.MUD) {
+                mudBlock++;
+            }
+        }
+
+        float laneWeight = weightFinish * finishBlock 
+                         + weightMud * mudBlock
+                         + weightOil * oilBlock
+                         + weightWall * wallBlock
+                         + weightTruck * truckBlock;
+
+        return laneWeight;
+    }
 }
